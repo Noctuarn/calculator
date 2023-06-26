e6ae3eabac23d38da15c55a646b6b53005481151
Roman Humenyak <romangumenyak2003@gmail.com>
2023-06-27 02:52:16 +0300

    create a markup

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  
+  return (
+    <>
+      <div className="calculator">
+        <div className="calculator__output">
+          <div className="calculator__prev-operand"></div>
+          <div className="calculator__curr-operand"></div>
+        </div>
+        <button className="calculator__button calculator__button_big">AC</button>
+        <button className="calculator__button">DEL</button>
+        <button className="calculator__button">/</button>
+        <button className="calculator__button">1</button>
+        <button className="calculator__button">2</button>
+        <button className="calculator__button">3</button>
+        <button className="calculator__button">*</button>
+        <button className="calculator__button">4</button>
+        <button className="calculator__button">5</button>
+        <button className="calculator__button">6</button>
+        <button className="calculator__button">+</button>
+        <button className="calculator__button">7</button>
+        <button className="calculator__button">8</button>
+        <button className="calculator__button">9</button>
+        <button className="calculator__button">-</button>
+        <button className="calculator__button">.</button>
+        <button className="calculator__button">0</button>
+        <button className="calculator__button calculator__button_big">=</button>
+      </div>
+    </>
+  )
+}
+
+export default App
